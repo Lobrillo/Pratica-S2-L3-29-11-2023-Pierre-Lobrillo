a0528810bec4120eeb668b4e6fc1cf7afb4954e7
Lobrillo <152413971+Lobrillo@users.noreply.github.com>
2023-12-06 17:17:56 +0100

    Add files via upload

---
+++ S2L3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+	
+ // Dichiarazione delle variabili
+ double reale1, reale2, risultato;
+
+  // Input da parte dell'utente
+ printf("Inserisci il primo numero: ");
+ scanf("%d", &reale1)
+ 
+  printf("Inserisci il secondo numero: ");
+scanf("%d", &reale2)
+
+// calcolo risultato
+risultato = reale1 * reale2;
+
+ // Visualizzazione del risultato
+ printf("Il risultato della moltiplicazione è:risultato);
+ 
+ return 0;
+ }
+
+ 
+
+
